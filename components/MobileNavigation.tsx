d7c1769dbfbc083ef6a273ac1bd6049f4bade17b
Amorem <dev@amorem.com>
2024-11-10 21:55:59 +0100

    wip

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,3 +1,62 @@
-export default function MobileNavigation() {
-	return <div>MobileNavigation</div>;
+"use client";
+import {
+	Sheet,
+	SheetContent,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+export default function MobileNavigation({
+	ownerId,
+	accountId,
+	fullName,
+	avatar,
+	email,
+}: {
+	ownerId: string;
+	accountId: string;
+	fullName: string;
+	avatar: string;
+	email: string;
+}) {
+	const [open, setOpen] = useState(false);
+	const { pathname } = usePathname();
+	return (
+		<header className="mobile-header">
+			<Image
+				src="/assets/icons/logo-full-brand.svg"
+				alt="Logo"
+				width={120}
+				height={52}
+				className="h-auto"
+			/>
+
+			<Sheet open={open} onOpenChange={setOpen}>
+				<SheetTrigger>
+					<Image
+						src="/assets/icons/menu.svg"
+						alt="menu"
+						width={30}
+						height={30}
+					/>
+				</SheetTrigger>
+				<SheetContent className="shad-sheet h-screen px-3">
+					<SheetTitle>
+						<div className="header-user">
+							<Image
+								src={avatar}
+								alt="Avatar"
+								width={44}
+								height={44}
+								className="header-user-avatar"
+							/>
+						</div>
+					</SheetTitle>
+				</SheetContent>
+			</Sheet>
+		</header>
+	);
 }
